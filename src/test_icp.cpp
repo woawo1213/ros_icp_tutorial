72aab2dfea376d5cee23bd94571555a651252801
sjm1213 <jmshin@wonik.com>
2022-01-25 14:43:43 +0900

    add save pcd file

--- src/test_icp.cpp
+++ src/test_icp.cpp
@@ -9,10 +9,14 @@
 #include <pcl/conversions.h>
 #include <pcl/common/transforms.h>
 #include <pcl/registration/icp.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
 
 // PCL specific includes
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <Eigen/Geometry>
+#include <string>
 
 class ICPTester
 {
@@ -20,26 +24,29 @@ class ICPTester
     ros::Publisher dst_pub_; // destination
     ros::Publisher ali_pub_; // align
     ros::Publisher acc_pub_; // accumulate
+    ros::Publisher tgt_pub_;
     ros::NodeHandle nh_;
     ros::Subscriber sub_;
 
     pcl::PointCloud<pcl::PointXYZ> *acc_pc = new pcl::PointCloud<pcl::PointXYZ>; // accumulate point cloud 
     pcl::PointCloud<pcl::PointXYZ> *save_pc = new pcl::PointCloud<pcl::PointXYZ>; // save previous point cloud
     
-    Eigen::Matrix4f tf_sum;// 초기 depth frame tf or identity 넣어줘야하나?
+    Eigen::Matrix4f tf_mul;// 초기 depth frame tf or identity 넣어줘야하나?
+    std::string file_path="/home/jm/workspace/icp_ws/src/icp_tutorial/pcd/desk/";
+    
     int count=0;
 
 public:
     ICPTester()
     {
         //depth optical frame tf
-        // tf_sum<< 0, 0, 1, 0,
+        // tf_mul<< 0, 0, 1, 0,
         //         -1, 0, 0, 0,
         //          0,-1, 0, 0,
         //          0, 0, 0, 1;
 
         // identity
-        tf_sum<< 1, 0, 0, 0,
+        tf_mul<< 1, 0, 0, 0,
                  0, 1, 0, 0,
                  0, 0, 1, 0,
                  0, 0, 0, 1;
@@ -48,6 +55,7 @@ public:
         src_pub_ = nh_.advertise<sensor_msgs::PointCloud2> ("source_output", 1000);
         dst_pub_ = nh_.advertise<sensor_msgs::PointCloud2> ("destination_output", 1000);
         acc_pub_ = nh_.advertise<sensor_msgs::PointCloud2> ("accumulate_output", 1000);
+        tgt_pub_ = nh_.advertise<sensor_msgs::PointCloud2> ("tgt_output", 1000);
         sub_ = nh_.subscribe<sensor_msgs::PointCloud2> ("/astra/depth/points", 1, &ICPTester::cloud_cb, this);
     };
 
@@ -63,78 +71,97 @@ public:
         pcl::PCLPointCloud2 cloud_filtered2;
         pcl::PCLPointCloud2 cloud_filtered3;
         pcl::PCLPointCloud2 cloud_filtered4;
+        pcl::PCLPointCloud2 cloud_filtered5;
 
         pcl::PointCloud<pcl::PointXYZ>::Ptr src_pc(new pcl::PointCloud<pcl::PointXYZ>);
         pcl::PointCloud<pcl::PointXYZ>::Ptr dst_pc(new pcl::PointCloud<pcl::PointXYZ>);
         pcl::PointCloud<pcl::PointXYZ>::Ptr align(new pcl::PointCloud<pcl::PointXYZ>);
         pcl::PointCloud<pcl::PointXYZ>::Ptr tf_tgt(new pcl::PointCloud<pcl::PointXYZ>);
+        pcl::PointCloud<pcl::PointXYZ>::Ptr pcd (new pcl::PointCloud<pcl::PointXYZ>);
         
         sensor_msgs::PointCloud2 output;
         sensor_msgs::PointCloud2 output2;
         sensor_msgs::PointCloud2 output3;
         sensor_msgs::PointCloud2 output4;
+        sensor_msgs::PointCloud2 output5;
 
+        // std::string write_file_name = "desk.pcd";
+        // std::string counting = std::to_string(count);
+        // write_file_name="0"+counting+"_"+write_file_name;
         // Convert to PCL data type , 상자 까기
         pcl_conversions::toPCL(*cloud_msg, *cloud);
 
         // Perform the actual filtering
         pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
         sor.setInputCloud (cloudPtr);
-        sor.setLeafSize (0.15f, 0.15f, 0.15f);
+        sor.setLeafSize (0.1f, 0.1f, 0.1f);
         sor.filter (cloud_filtered);
 
-        //convert pcl::PCLPointCloud2 -> pcl::PointCloud<pcl::PointXYZ>
-        pcl::fromPCLPointCloud2(cloud_filtered, *dst_pc);
+        // //convert pcl::PCLPointCloud2 -> pcl::PointCloud<pcl::PointXYZ>
+        pcl::fromPCLPointCloud2(cloud_filtered, *src_pc);
         
         // icp
         // skip initial source point cloud input
         if((count > 0)&&(count < 1000))
         {
             ROS_INFO("count: %d",count);
-            *src_pc = *save_pc;
+            //allocate previous point cloud
+            *dst_pc = *save_pc;
 
             // icp
             pcl::IterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> icp;
-
             // Registration 시행
             // Set the input source and target
             icp.setInputSource(src_pc);
             icp.setInputTarget(dst_pc);
-            // Set the max correspondence distance to 5cm
-            // icp.setMaxCorrespondenceDistance(5);
-            // Set the transformation epsilon (criterion 2)
-            icp.setTransformationEpsilon(1e-8);
-            // Set the maximum number of iterations (criterion 1) registration 될때까지 반복횟수
-            icp.setMaximumIterations(50);
+            // Set the max correspondence distance, src dst 사이의 최대거리 설정, 결과에 영향을 많이줌
+            icp.setMaxCorrespondenceDistance(10);
+            // Set the transformation epsilon ,
+            icp.setTransformationEpsilon(1e-10);
+            // Set the maximum number of iterations,  registration 될때까지 반복횟수
+            icp.setMaximumIterations(1000);
             // Perform the alignment
             icp.align(*align);
 
-            // Eigen::Isometry3f::
+            
             // Obtain the transformation that aligned src_pc to align
             Eigen::Matrix4f src2dst = icp.getFinalTransformation();
-
+            // Eigen::Isometry 로 바꿔서도 해보기
+            
             double score = icp.getFitnessScore();
             bool is_converged = icp.hasConverged();
 
-            std::cout<<"icp matrix: "<<std::endl;
+            ROS_INFO("icp matrix");
             std::cout<<src2dst<<std::endl;
 
-            // ROS_INFO("XYZ: %f %f %f ",src2tgt(0,3),src2tgt(1,3),src2tgt(2,3));
-
-            std::cout<<"score: "<<score<<std::endl;
-            std::cout<<"converged: "<<is_converged<<std::endl;
+            ROS_INFO("score: %lf",score);//src dst 거리의 평균, 작을수록 icp good
+            // ROS_INFO("converged: ",is_converged);
             std::cout<<"###############################################"<<std::endl;
 
             //multiple tf
-            tf_sum = src2dst * tf_sum;
-            std::cout<<"tf matrix: "<<std::endl;
-            std::cout<< tf_sum <<std::endl;
+            // src2dst=src2dst.reverse();
+            tf_mul = tf_mul * src2dst;
+            ROS_INFO("tf matrix");
+            std::cout<< tf_mul <<std::endl;
+
+            //print camera pose
+            std::cout<<"###############################################"<<std::endl;
+            ROS_INFO("camera pose t x:%f y:%f z:%f", tf_mul(0,3), tf_mul(1,3), tf_mul(2,3));
+            // ROS_INFO("camera pose t: ");
+            // std::cout<<"w: "<<tf_mul.<<std::endl;
+
+            Eigen::Isometry3f i3f;
+            std::cout<<"transpose"<<std::endl;
+            std::cout<<tf_mul.transpose()<<std::endl;
+
+            std::cout<<"###############################################"<<std::endl;
 
             //tranform init source cloud
-            pcl::transformPointCloud(*dst_pc, *tf_tgt, tf_sum);
+            // tf_mul=tf_mul.reverse();
+            pcl::transformPointCloud(*align, *tf_tgt, tf_mul);
 
             //accumulate aling point cloud
-            acc_pc->header.frame_id = dst_pc->header.frame_id;
+            acc_pc->header.frame_id = src_pc->header.frame_id;
             *acc_pc += *tf_tgt;
             
         }
@@ -143,22 +170,27 @@ public:
         pcl::toPCLPointCloud2(*src_pc, cloud_filtered2);
         pcl::toPCLPointCloud2(*align, cloud_filtered3);
         pcl::toPCLPointCloud2(*acc_pc, cloud_filtered4);
+        pcl::toPCLPointCloud2(*tf_tgt, cloud_filtered5);
 
         //convert convert pcl::PCLPointCloud2 -> sensor_msgs::PointCloud2
         pcl_conversions::moveFromPCL(cloud_filtered, output);
         pcl_conversions::moveFromPCL(cloud_filtered2, output2);
         pcl_conversions::moveFromPCL(cloud_filtered3, output3);
         pcl_conversions::moveFromPCL(cloud_filtered4, output4);
+        pcl_conversions::moveFromPCL(cloud_filtered5, output5);
 
         //publish
         dst_pub_.publish(output);
         src_pub_.publish(output2);
         ali_pub_.publish(output3);
         acc_pub_.publish(output4); 
+        tgt_pub_.publish(output5);
 
-        //save src point cloud
-        *save_pc = *dst_pc;
+        // pcl::io::savePCDFile<pcl::PointXYZ>(file_path+write_file_name,*dst_pc);
+        // save src point cloud
+        *save_pc = *src_pc;
         count++;
+        
     }
 
 };
@@ -169,7 +201,7 @@ int main (int argc, char** argv)
     ros::init (argc, argv, "test_icp");
 
     ICPTester it;
-    ros::Rate r(5);
+    ros::Rate r(1);
 
     while (ros::ok())
     {
