03dd0178aba8287aedeec45fad76b954db3e5b24
woawo1213 <woawo1213@gmail.com>
2022-03-15 18:03:43 +0900

    add roi axis

--- test/passthrough.cpp
+++ test/passthrough.cpp
@@ -2,37 +2,46 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
 #include <pcl/filters/passthrough.h>
+#include <pcl/point_cloud.h>
 
 //Filtering a PointCloud using a PassThrough filter
 //http://pointclouds.org/documentation/tutorials/passthrough.php#passthrough
 
 int main (int argc, char** argv)
 {
-    std::string read_file_name="table_top.pcd";
-    std::string write_file_name="table_top_passthrough.pcd";
-    std::string file_path="/home/jm/workspace/icp_ws/src/pcl_cpp_tutorial/pcd/";
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZRGB>);
+    std::string read_file_name="corner.pcd";
+    std::string write_file_name="corner_passthrough.pcd";
+    std::string file_path="/home/jm/workspace/icp_ws/src/icp_tutorial/pcd/";
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
 
     // *.PCD 파일 읽기 (https://raw.githubusercontent.com/adioshun/gitBook_Tutorial_PCL/master/Beginner/sample/tabletop.pcd)
-    pcl::io::loadPCDFile<pcl::PointXYZRGB> (file_path+read_file_name, *cloud);
+    pcl::io::loadPCDFile<pcl::PointXYZ> (file_path+read_file_name, *cloud);
 
     // 포인트수 출력
     std::cout << "Loaded :" << cloud->width * cloud->height  << std::endl;
 
     // 오브젝트 생성 
-    pcl::PassThrough<pcl::PointXYZRGB> pass;
-    pass.setInputCloud (cloud);                //입력 
-    pass.setFilterFieldName ("z");             //적용할 좌표 축 (eg. Z축)
-    pass.setFilterLimits (0.70, 1.5);          //적용할 값 (최소, 최대 값)
-    //pass.setFilterLimitsNegative (true);     //적용할 값 외 
-    pass.filter (*cloud_filtered);             //필터 적용 
+    pcl::PassThrough<pcl::PointXYZ> pass;
+    pass.setInputCloud (cloud);                
+    pass.setFilterFieldName ("x");
+    pass.setFilterLimits (0.3, 0.7);
+    pass.filter (*cloud_filtered);
+    // pass.setInputCloud (cloud_filtered); 
+    // pass.setFilterFieldName ("y");
+    // pass.setFilterLimits (0.0, 0.3);
+    // pass.filter (*cloud_filtered);
+    pass.setInputCloud (cloud_filtered);
+    pass.setFilterFieldName ("z");             
+    pass.setFilterLimits (0.7, 1.9);          
+    pass.setFilterLimitsNegative (true);     
+    pass.filter (*cloud_filtered);             
 
     // 포인트수 출력
     std::cout << "Filtered :" << cloud_filtered->width * cloud_filtered->height  << std::endl;  
 
     // 저장 
-    pcl::io::savePCDFile<pcl::PointXYZRGB>(file_path+write_file_name, *cloud_filtered); //Default binary mode save
+    pcl::io::savePCDFile<pcl::PointXYZ>(file_path+write_file_name, *cloud_filtered); //Default binary mode save
 
     return (0);
 }
